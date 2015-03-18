7c2eb1adffdbbacfa777d339389aa3eefa0bf351
Max Yeo <mzwyeo@gmail.com>
2015-03-18 09:15:56 -0400

    initial commit, boilerplate

---
+++ js/style.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {  
+
+    sizeitup();
+
+    //nav bar scoll action
+    $('').click(function() {
+        $("html, body").animate({ scrollTop: $('').offset().top}, 500);
+    });//clicked on top
+    
+});
+
+$(window).resize(function() {
+  sizeitup();
+});
+
+function sizeitup() {
+  var bodyh = $(window).height();
+  var bodyw = $(window).width();
+  if (bodyw > 650) {
+    $("").css("height",bodyh);
+  }
+}
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 0) {
+    $('header').addClass('up');
+  } else {
+    $('header').removeClass('up');
+  }
+});
+
+//SMOOTH SCROLLING
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+$(".navicon-button").click(function(){
+  $("nav").toggleClass("open");
+  $(this).toggleClass("open");
+});
+
+$(".member .member-pic").click(function(e){
+  e.stopPropagation();
+  var member = $(this).parent(".member");
+  member.children(".member-bio").addClass("open");
+});
+
+$(".member-out").click(function(e){
+  e.stopPropagation();
+  $(".member-bio").removeClass("open");
+});
